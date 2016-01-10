a30682f28f0082376a5aa45f256c815e50f9a246
wmhall001 <wmhall001@gmail.com>
2016-01-10 14:14:01 -0500

    Front end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthay is soon.'
   end
 
 end
